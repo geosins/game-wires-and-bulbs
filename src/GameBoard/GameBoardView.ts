3378e70e5c52e5c9cc50a10f78ae4a53bbabe5f0
George Kosturov <geosins@yandex.ru>
2021-09-22 14:56:50 +0300

    Refactor GameBoardView

--- src/GameBoard/GameBoardView.ts
+++ src/GameBoard/GameBoardView.ts
@@ -1,42 +1,51 @@
 import { Square } from './Square';
+import { Utils } from '../Utils';
+
+const ROOT_CLASS_NAME = 'game-board';
 
 export class GameBoardView {
-    private squaresView: HTMLElement[][];
+    private classNames = {
+        root: ROOT_CLASS_NAME,
+        square: `${ROOT_CLASS_NAME}__square`,
+    }
+
+    private elements;
 
     constructor(squares: Square[][], onClick: (x: number, y: number) => void) {
-        this.initSquareView(squares);
+        this.initElements(squares);
         this.addClickHandler(onClick);
     }
 
-    public render(): void {
-        const root = document.getElementsByClassName('game-board')[0];
-        root.innerHTML = '';
+    public render(): HTMLElement {
+        const { root, squares } = this.elements;
 
-        for (let y = 0; y < this.squaresView[0].length; y++) {
-            for (let x = 0; x < this.squaresView.length; x++) {
-                root.appendChild(this.squaresView[x][y])
+        for (let y = 0; y < squares[0].length; y++) {
+            for (let x = 0; x < squares.length; x++) {
+                root.appendChild(squares[x][y])
             }
             root.appendChild(document.createElement('br'))
         }
+
+        return root;
     }
 
-    private initSquareView(squares: Square[][]): void {
-        this.squaresView = squares.map((column, x) => column.map((square, y) => {
-            const element = document.createElement('div');
-            element.className = 'field';
-            element.appendChild(square.render())
-            return element;
+    private initElements(squares: Square[][]): void {
+        this.elements = {
+            root: Utils.createElement('div', this.classNames.root),
+            squares: this.createSquareWrappers(squares),
+        }
+    }
+
+    private createSquareWrappers(squares: Square[][]): HTMLElement[][] {
+        return squares.map(row => row.map(square => {
+            return Utils.createElement('div', this.classNames.square, [square.render()]);
         }))
     }
 
     private addClickHandler(onClick: (x: number, y: number) => void): void {
-        for (let y = 0; y < this.squaresView[0].length; y++) {
-            for (let x = 0; x < this.squaresView.length; x++) {
-                this.squaresView[x][y].addEventListener('click', () => {
-                    onClick(x, y);
-                });
-            }
-        }
+        this.elements.squares.forEach((row, x) => row.forEach((square, y) => {
+            square.addEventListener('click', () => onClick(x, y));
+        }))
     }
 
 }
